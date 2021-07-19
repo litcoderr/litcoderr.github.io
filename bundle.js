588a16612efeacbca72f32c32df4e6fc001b24f1
Youngchae James Chee <litcoderr@gmail.com>
2021-07-19 16:35:45 +0900

    master: add image

--- bundle.js
+++ bundle.js
@@ -106,7 +106,7 @@ eval("\nvar __importDefault = (this && this.__importDefault) || function (mod) {
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-eval("\nvar __importDefault = (this && this.__importDefault) || function (mod) {\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\n};\nObject.defineProperty(exports, \"__esModule\", { value: true });\nvar react_1 = __importDefault(__webpack_require__(/*! react */ \"./node_modules/react/index.js\"));\nfunction ProfilePic() {\n    return (react_1.default.createElement(\"div\", null,\n        react_1.default.createElement(\"img\", null)));\n}\nexports.default = ProfilePic;\n\n\n//# sourceURL=webpack:///./client/components/profilepic.tsx?");
+eval("\nvar __importDefault = (this && this.__importDefault) || function (mod) {\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\n};\nObject.defineProperty(exports, \"__esModule\", { value: true });\nvar react_1 = __importDefault(__webpack_require__(/*! react */ \"./node_modules/react/index.js\"));\nfunction ProfilePic() {\n    return (react_1.default.createElement(\"div\", null,\n        react_1.default.createElement(\"img\", { src: \"https://www.github.com/litcoderr/litcoderr.github.io/blob/master/client/src/profile1.jpg\" })));\n}\nexports.default = ProfilePic;\n\n\n//# sourceURL=webpack:///./client/components/profilepic.tsx?");
 
 /***/ }),
 
--- client/components/profilepic.tsx
+++ client/components/profilepic.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function ProfilePic() {
     return (
         <div>
-            <img></img>
+            <img src="https://www.github.com/litcoderr/litcoderr.github.io/blob/master/client/src/profile1.jpg"></img>
         </div>
     )
 }
