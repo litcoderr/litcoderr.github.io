391d7a44f907576a7b3ccbda4b7613c5e2a39bfc
jameschee <litcoderr@gmail.com>
2024-05-20 17:42:43 +0900

    updated profile pic

--- client/components/profilepic.tsx
+++ client/components/profilepic.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function ProfilePic() {
     return (
         <div id="prof">
-            <img id="profimg" src="https://raw.githubusercontent.com/litcoderr/litcoderr.github.io/master/client/src/profile1.jpg"></img>
+            <img id="profimg" src="https://raw.githubusercontent.com/litcoderr/litcoderr.github.io/master/client/src/profile.jpg"></img>
         </div>
     )
 }
